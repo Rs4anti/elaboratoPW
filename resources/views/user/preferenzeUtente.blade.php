121756a5b910d9358c764eb92b297b8038ce0d27
Rs4anti <44727222+Rs4anti@users.noreply.github.com>
2024-06-26 16:12:50 +0200

    validazione input prefrenze ok

--- resources/views/user/preferenzeUtente.blade.php
+++ resources/views/user/preferenzeUtente.blade.php
@@ -1,8 +1,40 @@
 @extends('layouts.master')
 
-@section('title', 'Le tue preferenze')
+@section('title')
+    @if($registiPreferiti->isEmpty() && $generiPreferiti->isEmpty())
+        Non hai espresso le tue preferenze su registi e generi!
+    @else
+        Le tue preferenze.
+@endif
+@endsection
 
 @section('body')
+
+<script>
+    $(document).ready(function(){
+            $("form").submit(function(event){
+
+                // Verifica se almeno una categoria è stata selezionata
+                if ($("input[name='generi[]']:checked").length === 0) {
+                    $("#invalid-genere").text("Selezionare almeno un genere preferito.");
+                    event.preventDefault(); // Impedisce l'invio del modulo
+                    $("input[name='generi[]']").focus();
+                } else {
+                    $("#invalid-genere").text("");
+                }
+
+                // Verifica se almeno un regista è stata selezionato
+                if ($("select[name='registi[]'] option:selected").length === 0) {
+                    $("#invalid-regista").text("Selezionare almeno un regista preferito.");
+                    event.preventDefault(); // Impedisce l'invio del modulo
+                    $("select[name='registi[]']").focus();
+                } else {
+                    $("#invalid-regista").text("");
+                }
+
+        });
+    });
+</script>
 
 <div class="row">
         <div class="col-md-12">
@@ -14,10 +46,6 @@
         @endif
             @csrf
 
-            @if(!$registiPreferiti->isEmpty() || !$generiPreferiti->isEmpty())
-            <div>SEI IN MODIFICA</div>
-            @endif
-            
                 <div class="form-group row mb-3">
                     <div class="col-md-2">
                         <label for="registi">Registi Preferiti</label>
